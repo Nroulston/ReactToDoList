51ab41d2737b60c1326c4554789f4f006dc83d53
Nroulston <nicolas.roulston@gmail.com>
2020-09-28 12:54:13 -0700

    check box component mocked, Parent component mocked

---
+++ src/CheckBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class CheckBox extends Component {
+  render() {
+    return (
+      <div>
+        <input type="checkbox"/>
+        <p>placeholder text</p>
+      </div>
+    )
+  }
+}
+
+export default CheckBox
---
+++ src/TodoCheckBoxes.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import CheckBox from './CheckBox'
+
+class TodoCheckBoxes extends Component {
+  render() {
+    return (
+      <div>
+        <CheckBox />
+        <CheckBox />
+        <CheckBox />
+
+      </div>
+    )
+  }
+}
+
+export default TodoCheckBoxes
